ac63e0565f0dfb3727cb095f5f6598866b03997c
Gopal Anand <32189444+gopalanand333@users.noreply.github.com>
2023-09-01 09:52:50 +0530

    Update handler.js
    Fixed Serverless code structure to adapt to Latest kyma standards

--- qrcodegenerator/handler.js
+++ qrcodegenerator/handler.js
@@ -1,17 +1,18 @@
 const httpclientaxios = require("./httpclientaxios");
 
-module.exports = async function (event, context) {
+module.exports = {
+	main: async function (event, context) {
 		let msg;
 		try {
 			msg = JSON.parse(event.data);
 		} catch (e) {
-		   if(event.data){
-				msg =  event.data
-			       console.log("msg", msg.data);
-		  	}else{
-				  return "Looks like there is some issue with the data format. Expected data format: JSON/String";
-			  }
+			if (event.data) {
+				msg = event.data
+			} else {
+				return "Looks like there is some issue with the data format. Expected data format: JSON/String";
+			}
 		}
 		const response = await httpclientaxios.postImage(context, msg, event);
 		return "hello";
+	}
 }
